840d5b9ff771a92002f8fa35779d1af7ee06c219
Talman95 <taltro528@gmail.com>
2023-06-28 14:28:51 +0300

    config decomposition

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,13 @@
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
+    return [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: options.paths.html
+        }),
+    ]
+
+}
---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,24 @@
+import {BuildOptions} from "./types/config";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildPlugins} from "./buildPlugins";
+import webpack from "webpack";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { mode, paths } = options;
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+        plugins: buildPlugins(options),
+    }
+}
