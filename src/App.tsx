972498373d88afc15ab5e8a3bcc0f169308e03ef
Talman95 <taltro528@gmail.com>
2023-06-30 19:54:59 +0300

    setting css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {Counter} from "./components/Counter";
 import './index.scss';
 
 const App = () => {
-    const [value, setValue] = useState(0)
-
-    const increment = () => {
-        setValue(prev => prev + 1)
-    }
-
     return (
-        <div>
-            <h1>{value}</h1>
-            <button onClick={increment}>+</button>
+        <div className={'app'}>
+            <Counter/>
         </div>
     );
 };
