1fc90ef93b57a4b312736545ac1054b1d7ecf504
Talman95 <taltro528@gmail.com>
2023-07-01 18:54:31 +0300

    add classNames.ts

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,11 @@
+type Mods = Record<string, boolean | string>
+
+export const classNames = (className: string, mods: Mods  = {}, additional: string[] = []): string => {
+    return [
+        className,
+        ...Object.entries(mods)
+            .filter(([_, condition]) => Boolean(condition))
+            .map(([cls]) => cls),
+        ...additional,
+    ].join(' ')
+}
