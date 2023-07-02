77cf34a3faa94612f271bf430abb80e6045ff0a5
Talman95 <taltro528@gmail.com>
2023-07-02 23:23:27 +0300

    add feature-sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from 'react';
 import {Route, Routes, Link} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 import './styles/index.scss';
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "./providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
@@ -18,8 +18,8 @@ const App = () => {
             </nav>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
                 </Routes>
             </Suspense>
         </div>
