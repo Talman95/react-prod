79adfe7949a3eea5062626bab2beb0f675160865
Talman95 <taltro528@gmail.com>
2023-07-06 12:49:54 +0300

    add jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,35 @@
+import { classNames } from './classNames';
+
+describe('classNames test', () => {
+  test('add some class', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional classes', () => {
+    const result = 'someClass class1 class2';
+
+    expect(classNames('someClass', {}, ['class1', 'class2']))
+      .toBe(result);
+  });
+
+  test('with mods true', () => {
+    const result = 'someClass hovered class1 class2';
+
+    expect(classNames('someClass', { hovered: true }, ['class1', 'class2']))
+      .toBe(result);
+  });
+
+  test('with mods false', () => {
+    const result = 'someClass hovered class1 class2';
+
+    expect(classNames('someClass', { hovered: true, scrollable: false }, ['class1', 'class2']))
+      .toBe(result);
+  });
+
+  test('with mods undefined', () => {
+    const result = 'someClass hovered class1 class2';
+
+    expect(classNames('someClass', { hovered: true, scrollable: undefined }, ['class1', 'class2']))
+      .toBe(result);
+  });
+});
